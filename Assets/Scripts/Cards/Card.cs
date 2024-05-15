6623f58a2a76cdbdff7c2ccc8eea1eff06ebde0e
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-05-15 17:02:43 -0500

    Set Up Card Script
    The Card script is just going to house card info,. This info is set by the object in the scene. So, hopefully,  each card will have different stats since they are different for each preset card.

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public string CardName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    public string CardDamage;
+
+    [SerializeField]
+    public string CardElement;
+
+    [SerializeField]
+    public bool isSpell;
+
+    [SerializeField]
+    public int SpellTier;
 }
