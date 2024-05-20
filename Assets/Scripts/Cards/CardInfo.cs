d07f57caf7c2dcabf393d3695f04c07861aebc50
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-05-20 13:58:55 -0500

    Made Custom Inspector for CardInfo

--- Assets/Scripts/Cards/CardInfo.cs
+++ Assets/Scripts/Cards/CardInfo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CardInfo : MonoBehaviour
@@ -19,4 +20,24 @@ public class CardInfo : MonoBehaviour
 
     [SerializeField]
     public int SpellTier;
+
+
+    [CustomEditor(typeof(CardInfo))]
+    public class CardInfoEditor : Editor{
+        override public void OnInspectorGUI(){
+            var CardInfo = target as CardInfo;
+
+            CardInfo.CardName = EditorGUILayout.TextField("Name of Card", CardInfo.CardName);
+            CardInfo.CardDamage = EditorGUILayout.TextField("Damage of the Card", CardInfo.CardDamage);
+            CardInfo.CardElement = EditorGUILayout.TextField("Element of the Card", CardInfo.CardElement);
+
+            CardInfo.isSpell = EditorGUILayout.Toggle("Is Spell", CardInfo.isSpell);
+
+            if(CardInfo.isSpell){
+                using(new EditorGUI.DisabledScope(!CardInfo.isSpell)){
+                    CardInfo.SpellTier = EditorGUILayout.IntField("Spell Tier", CardInfo.SpellTier);
+                }
+            }
+        }
+    }
 }
