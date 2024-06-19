1d6b2cc5d35385288c39bc3022704b099177ca6f
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-06-19 14:10:43 -0500

    Removed a Method that didn't work

--- Assets/Scripts/Turns/TurnManager.cs
+++ Assets/Scripts/Turns/TurnManager.cs
@@ -40,9 +40,9 @@ public class TurnManager : MonoBehaviour{
         //Initiative is tied to the player and enemy
         //Make a list of everyone and sort from Greatest to Least
         InitiativeList = GetAll.GetInitiativeOrder();
-        //Debug.Log(InitiativeList.Count);
 
         StartCoroutine(GetttingPlayerHand());
+        StopCoroutine(GetttingPlayerHand());
     }
 
     IEnumerator GetttingPlayerHand(){
@@ -50,25 +50,7 @@ public class TurnManager : MonoBehaviour{
         cardManager.GetPlayerHand();
     }
 
-    IEnumerator goingThroughTurnOrder(){
-        for(int i = 0; i < InitiativeList.Count; i++){
-            //Debug.Log(InitiativeList[i] + "'s Turn" + "\nIndex: " + i);
-
-            //Check to see if the current unit is a Player or Enemy
-            if(InitiativeList[i] is Enemy){
-                //The AI needs to make a move for the forloop to move on.
-                Debug.Log("Waiting for Enemy");
-                yield return new WaitForSeconds(30);
-                Debug.Log("Enemy's Turn is Done");
-            }else if(InitiativeList[i] is Player){
-                //The forloop needs to wait for the player to hit the End Turn button before it is allowed to continue
-                Debug.Log("Waiting for Player");
-                yield return new WaitForSeconds(10);
-                Debug.Log("Player's Turn is Done");
-
-            }
-        }
-    }
+    
 
 
     public void EndTurn(){
